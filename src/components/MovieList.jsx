32141cca8eed3ac65370ddf2b11ba9c2160ec0cb
cristianqsanchez <cristianquintero316@gmail.com>
2023-03-19 17:42:25 -0500

    refactor: :recycle: use movie component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,11 @@
+import Movie from './Movie'
+
 export default function MovieList ({ movies }) {
   return (
     <ul>
       {
         movies.map(movie => (
-          <li key={movie.id}>
-            <h3>{movie.title}</h3>
-            <span>{movie.year}</span>
-            <img src={movie.image} alt={movie.title} />
-          </li>
+          <Movie key={movie.id} data={movie} />
         ))
       }
     </ul>
