6fb854047a1383ec4d8724edb50b513b63477c85
cristianqsanchez <cristianquintero316@gmail.com>
2023-03-21 19:44:36 -0500

    feat: :sparkles: add movie list styles

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,7 +2,7 @@ import Movie from './Movie'
 
 export default function MovieList ({ movies }) {
   return (
-    <ul>
+    <ul className='grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-4 justify-center place-items-center'>
       {
         movies.map(movie => (
           <Movie key={movie.id} data={movie} />
