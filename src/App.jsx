d70d03857c832150c83d9ecd75e304e1ffeab3d5
cristianqsanchez <cristianquintero316@gmail.com>
2023-03-21 19:56:21 -0500

    refactor: :recycle: more clean app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import './App.css'
-import { useState } from 'react'
-import useMovies from './hooks/useMovies'
-import MovieList from './components/MovieList'
+import Search from './components/Search'
 
 function App () {
-  const [search, setSearch] = useState('')
-  const { movies, loading, getMovies } = useMovies({ search })
-  const handleChange = (e) => {
-    const newSearch = e.target.value
-    setSearch(newSearch)
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    getMovies({ search })
-  }
   return (
     <main>
-      <form
-        onSubmit={handleSubmit}
-      >
-        <input
-          onChange={handleChange}
-          value={search}
-        />
-      </form>
-      <section>
-        {loading ? <p>Loading...</p> : <MovieList movies={movies} />}
-      </section>
+      <Search />
     </main>
   )
 }
