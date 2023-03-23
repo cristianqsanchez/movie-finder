e30e1eed1b8c750f41cf97c19f1fa5564d365533
cristianqsanchez <cristianquintero316@gmail.com>
2023-03-22 19:35:40 -0500

    build: :green_heart: add deploy url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://cristianqsanchez.github.io/movie-finder/',
   plugins: [react()]
 })
