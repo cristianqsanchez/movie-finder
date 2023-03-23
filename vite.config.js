6c12e8f89041657439bd33ebf16e1b592e643070
cristianqsanchez <cristianquintero316@gmail.com>
2023-03-22 19:50:17 -0500

    fix: :green_heart: correct deploy base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://cristianqsanchez.github.io/movie-finder/',
+  base: '/movie-finder/',
   plugins: [react()]
 })
